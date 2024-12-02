737f441eaf1fa4a81377f9dd3473d6ddfa67acec
Sacha Vandermoeten <sachadu835@gmail.com>
2024-12-02 19:08:05 +0100

    Ajout Header

---
+++ src/components/pageStructure/Header.jsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+const Container = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 20px;
+`;
+const HeaderLink = styled.a`
+  color: var(--firt-font-color);
+  font-size: 13px;
+  text-decoration: none;
+`;
+const HeaderLogo = styled.p`
+  font-size: 25px;
+  margin: 0;
+`;
+
+function Header() {
+  return (
+    <Container>
+      <HeaderLogo>LOGO</HeaderLogo>
+      <HeaderLink href="#">Nous contacter</HeaderLink>
+    </Container>
+  );
+}
+
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import GlobalStyle from "./helpers/GlobalStyle.jsx";
 import Homepage from "./pages/Homepage.jsx";
+import Header from "./components/pageStructure/Header.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <GlobalStyle />
+    <Header />
     <Homepage />
   </StrictMode>
 );
